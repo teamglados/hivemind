c002edde46c6f86b2b41224151917409972dddb0
Andreas Urbanski <urbanski.andreas@gmail.com>
2020-11-07 20:15:54 +0200

    Fix formatting

--- server/hivemind/runtime.py
+++ server/hivemind/runtime.py
@@ -1,11 +1,10 @@
 from sqlalchemy.sql import text
 from sqlalchemy.ext.asyncio import create_async_engine
-import sqlalchemy
 from sqlalchemy import MetaData
+import sqlalchemy
 import asyncio
 
 from hivemind.config import *
-from hivemind.config import SQLALCHEMY_DATABASE_URI
 
 metadata = MetaData()
 engine = create_async_engine(str(SQLALCHEMY_DATABASE_URI), echo=True)
