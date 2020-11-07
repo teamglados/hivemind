42cff4c5320520fb9b6b56ca48ff0f94c2f24a21
Ville Toiviainen <villej.toiviainen@gmail.com>
2020-11-07 16:03:31 +0200

    update config

--- server/hivemind/config.py
+++ server/hivemind/config.py
@@ -10,7 +10,7 @@ config = Config(".env")
 
 # database
 DATABASE_HOSTNAME = config("DATABASE_HOSTNAME", default="localhost")
-DATABASE_CREDENTIALS = config("DATABASE_CREDENTIALS", cast=Secret, default="postgres:postgres")
+DATABASE_CREDENTIALS = config("DATABASE_CREDENTIALS", cast=Secret, default="admin:admin")
 DATABASE_NAME = config("DATABASE_NAME", default="prod")
 DATABASE_PORT = config("DATABASE_PORT", default="5432")
 SQLALCHEMY_DATABASE_URI = f"postgresql+asyncpg://{DATABASE_CREDENTIALS}@{DATABASE_HOSTNAME}:{DATABASE_PORT}/{DATABASE_NAME}"
