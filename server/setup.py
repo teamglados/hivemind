e18d8c92994dd81e6d27a2edf392aab7ef8d6102
Andreas Urbanski <urbanski.andreas@gmail.com>
2020-11-07 12:47:36 +0200

    Add package setup

---
+++ server/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    version='0.0.1',
+    name='hivemind',
+    packages=['hivemind'],
+    description='Hivemind API',
+    author='Andreas Urbanski',
+    author_email='urbanski.andreas@gmail.com',
+    python_requires='>=3.8.2',
+    license='TBD',
+    install_requires=[],
+    classifiers=[
+        'License :: Other/Proprietary License',
+        'Private :: Do Not Upload to pypi server',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
