4c87ac6224565438bf80cb6d4bf8bc0830f4b785
Andreas Urbanski <urbanski.andreas@gmail.com>
2020-11-07 15:09:33 +0200

    Update config

--- server/hivemind/config.py
+++ server/hivemind/config.py
@@ -14,3 +14,9 @@ DATABASE_CREDENTIALS = config("DATABASE_CREDENTIALS", cast=Secret, default="post
 DATABASE_NAME = config("DATABASE_NAME", default="prod")
 DATABASE_PORT = config("DATABASE_PORT", default="5432")
 SQLALCHEMY_DATABASE_URI = f"postgresql+asyncpg://{DATABASE_CREDENTIALS}@{DATABASE_HOSTNAME}:{DATABASE_PORT}/{DATABASE_NAME}"
+
+JWT_ADMIN_SECRET = config("JWT_ADMIN_SECRET", default="90XNxHARHo7Zmd7gjc9YUEatR428ZCLE")
+JWT_SECRET = config("JWT_SECRET", default="9YUEatR428ZCLE")
+
+HTTP_HOST = config("HTTP_HOST", default="localhost")
+HTTP_PORT = config("HTTP_PORT", default=8000)
