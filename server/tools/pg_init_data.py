59b7880a1cc8e83b3006dae74e6b14a3d3f2b073
Ville Toiviainen <villej.toiviainen@gmail.com>
2020-11-07 19:08:29 +0200

    insert all

--- server/tools/pg_init_data.py
+++ server/tools/pg_init_data.py
@@ -1,59 +1,30 @@
 import asyncio
+from sqlalchemy import MetaData
+from sqlalchemy.sql import select
+from sqlalchemy import MetaData
 
-from sqlalchemy.sql import text
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from hivemind.models.user import User
-from hivemind.models.hint import Hint
-from hivemind.models.hint_item import HintItem
-from hivemind.models.question import Question
-from hivemind.models.answer import Answer
-from hivemind.models.message import Message
-from hivemind.models.discussion_item import DiscussionItem
+from hivemind.models import *
 from hivemind.config import SQLALCHEMY_DATABASE_URI
+from hivemind.runtime import metadata, engine
 
-engine = create_async_engine(str(SQLALCHEMY_DATABASE_URI), echo=True)
 
 async def pg_init_tables():
     async with engine.begin() as conn:
-        # question
-        await conn.run_sync(Question.metadata.drop_all)
-        await conn.run_sync(Question.metadata.create_all)
+        print(metadata.sorted_tables)
+        await conn.run_sync(metadata.drop_all)
+        await conn.run_sync(metadata.create_all)
 
-        # user
-        await conn.run_sync(User.metadata.drop_all)
-        await conn.run_sync(User.metadata.create_all)
+    async with engine.connect() as conn:
 
-        # hint
-        await conn.run_sync(Hint.metadata.drop_all)
-        await conn.run_sync(Hint.metadata.create_all)
+        await conn.execute(
+            User.insert(), [{"name": "some name 1"}, {"name": "some name 2"}]
+        )
 
-        # answer
-        await conn.run_sync(Answer.metadata.drop_all)
-        await conn.run_sync(Answer.metadata.create_all)
+        result = await conn.execute(select(User).where(User.c.name == "some name 1"))
+        result = await conn.execute(select(User).where(User.c.name == "some name 1"))
 
-        # message
-        await conn.run_sync(Message.metadata.drop_all)
-        await conn.run_sync(Message.metadata.create_all)
+        return result.fetchall()
 
-        # Discussion item
-        await conn.run_sync(DiscussionItem.metadata.drop_all)
-        await conn.run_sync(DiscussionItem.metadata.create_all)
-
-        # Hint item
-        await conn.run_sync(HintItem.metadata.drop_all)
-        await conn.run_sync(HintItem.metadata.create_all)
-
-async def pg_create_test():
-    async with AsyncSession(engine) as session:
-        async with session.begin():
-            session.add_all(
-                [
-                    User(name="test"),
-                ]
-            )
-        await session.commit()
 
 if __name__ == '__main__':
     asyncio.run(pg_init_tables())
