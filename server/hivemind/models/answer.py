8cd8847c9910b5b9c75bc6434b37085f188e73c7
Ville Toiviainen <villej.toiviainen@gmail.com>
2020-11-07 21:06:06 +0200

    not needed

--- server/hivemind/models/answer.py
+++ server/hivemind/models/answer.py
@@ -7,7 +7,7 @@ from hivemind.runtime import metadata
 Answer = Table("answer", metadata,
     Column("id", Integer, primary_key=True),
     Column("value", String, nullable=False),
-    Column("score", Float, nullable=False),
+    Column("score", Float, nullable=True),
     Column("user_id", Integer, ForeignKey("user.id"), nullable=False),
     Column("question_id", Integer, ForeignKey("question.id"), nullable=False),
     Column("created_at", DateTime, default=datetime.utcnow)
