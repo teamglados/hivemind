e4827234111b4e452993fa8ccf09b89e39b1f528
Ville Toiviainen <villej.toiviainen@gmail.com>
2020-11-07 19:58:06 +0200

    init questions and answers

--- server/tools/pg_init_data.py
+++ server/tools/pg_init_data.py
@@ -16,15 +16,45 @@ async def pg_init_tables():
 
     async with engine.connect() as conn:
 
+        result = await conn.execute(
+            User.insert().returning(User.c.id), [{"name": "some name 1"}],
+        )
+        user_id = result.fetchone()[0]
+
+        result = await conn.execute(
+            Question.insert().returning(Question.c.id),
+            [{"question": "some name 1", "answer": "answer", "score": 100}],
+        )
+        question_id = result.fetchone()[0]
+
         await conn.execute(
-            User.insert(), [{"name": "some name 1"}, {"name": "some name 2"}]
+            Answer.insert(),
+            [
+                {
+                    "value": "wrong",
+                    "score": 0,
+                    "user_id": user_id,
+                    "question_id": question_id,
+                },
+                {
+                    "value": "answer",
+                    "score": 100,
+                    "user_id": user_id,
+                    "question_id": question_id,
+                },
+            ],
         )
 
-        result = await conn.execute(select(User).where(User.c.name == "some name 1"))
-        result = await conn.execute(select(User).where(User.c.name == "some name 1"))
+        result = await conn.execute(select(Question))
+        results = result.fetchall()
+        print(results)
 
-        return result.fetchall()
+        result = await conn.execute(select(User))
+        results = result.fetchall()
+        print(results)
+        await conn.commit()
+        return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(pg_init_tables())
