99394749eb6f69d27fbb59c41890c7eeaf538766
Ville Toiviainen <villej.toiviainen@gmail.com>
2020-11-08 00:37:44 +0200

    add active discussion id

--- server/hivemind/models/user.py
+++ server/hivemind/models/user.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Integer, MetaData, Table
+from sqlalchemy.dialects.postgresql import UUID
 
 from hivemind.runtime import metadata
 
@@ -9,5 +10,6 @@ User = Table("user", metadata,
     Column("name", String, nullable=False),
     Column("active_question_id", Integer, ForeignKey("question.id"), nullable=True),
     Column("active_question_last_active", DateTime, nullable=True),
+    Column("active_discussion_id", UUID(as_uuid=True), nullable=True),
     Column("created_at", DateTime, default=datetime.utcnow)
 )
